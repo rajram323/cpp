4ea2f26b992f448069b34aa3f7ae07a1bd4e6861
rajram323 <62275006+rajram323@users.noreply.github.com>
2025-06-14 22:47:24 +0530

    Update test42.cpp

--- test42.cpp
+++ test42.cpp
@@ -29,3 +29,42 @@ int main()
     cout << v.x << " " << v.y << " " << v.z << endl;
     return 0;
 }
+
+//
+
+#include <iostream>
+#include <string>
+#include <tuple>
+using namespace std;
+
+// second example
+std::tuple<int, char, std::string> fun(bool flg)
+{
+    if (flg)
+        return make_tiple(1, 'x', "rupesh");
+    else
+        return make_tiple(1, 'x', "rupesh");
+}
+
+int main()
+{
+    int num;
+    char code;
+    string name;
+
+    tie(num, code, name) = fun(true);
+    cout << num << " " << code << " " << name << endl;
+    tie(num, code, name) = fun(false);
+    cout << num << " " << code << " " << name << endl;
+    return 0;
+
+    //
+    int num;
+    char code;
+    string name;
+    auto v = fun(true);
+    num = std::get<0>(v);
+    code = std::get<1>(v);
+    name = std::get<2>(v);
+    cout << num << " " << code << " " << name << endl;
+}
