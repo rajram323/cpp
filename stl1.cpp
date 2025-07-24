9392e32aa6611282ff9ed6b0a026b4f8d5f058b9
rajram323 <62275006+rajram323@users.noreply.github.com>
2025-07-24 23:21:01 +0530

    stl in c++

---
+++ stl1.cpp
@@ -0,0 +1,6 @@
+// TOPIC: STL Algorithms
+
+// NOTES:
+// 0. STL algorithms library defines functions for a variety of purposes (e.g. searching, sorting, counting, manipulating) 
+// 1. Algorithms are applied to range of elements.
+// 3. <algorithm> header is used to get all algorithms in stl.
