ad8cd7b27f7da5d73b9b093753d6437b68013dd2
rajram323 <62275006+rajram323@users.noreply.github.com>
2025-08-06 10:57:45 +0530

    Container Inside Container In C++

---
+++ stl_6.cpp
@@ -0,0 +1,29 @@
+yt - https://www.youtube.com/watch?v=2QR34zwjWUA&list=PLk6CEY9XxSIA-xo3HRYC3M0Aitzdut7AA&index=6
+// TOPIC: Container Inside Container
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    std::vector<vector<int>> Tree;
+    int edge, n1, n2;
+
+    cin >> edge;
+    Tree.resize(edge);
+
+    for (int i = 0; i < edge; ++i) {
+        cin >> n1 >> n2;
+        Tree[n1].push_back(n2);
+    }
+    
+    for (const auto &e1: Tree) {
+        for (const auto &e2: e1) {
+            cout << e2;
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
