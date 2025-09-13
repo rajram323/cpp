2c7c75e09daa07495c5f51732dc6bfcd1104d308
rajram323 <62275006+rajram323@users.noreply.github.com>
2025-09-13 14:31:36 +0530

    binary search

---
+++ stl_algo_11.cpp
@@ -0,0 +1,21 @@
+Binary Search
+yt  - https://www.youtube.com/watch?v=S2x2_tWvm7w&list=PLk6CEY9XxSIBPZCfQiphw4221uSbp0Q75&index=11 
+git - https://github.com/cppnuts-yt/CppNuts/blob/master/C%2B%2B%20Programming/STL/STL%20Algorithms/binary%20search.cpp
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    
+    std::vector<int> Vec {1, 3, 4, 5, 9};
+    std::vector<int> elements {1, 2, 3};
+ 
+    for (auto e : elements) {
+        cout << "Searching for " << e << '\n';
+        if (std::binary_search(Vec.begin(), Vec.end(), e))
+            cout << "Found " << e << endl;
+        else
+            cout << "Not found!" << endl;
+    }
+}
